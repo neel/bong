8791d5d29025e61bbdbb780967d5fd90ddc05cca
Neel Basu <neel.basu.z@gmail.com>
2012-04-22 22:46:07 +0530

    Issue #24 resolved added DummyXDO

---
+++ usr/lib/module/xdo/DummyXDO.php
@@ -0,0 +1,14 @@
+<?php
+final class DummyXDO extends AbstractXDO{
+	public function serialized(){
+		return false;
+	}
+	protected function fileName(){}
+	public function serialize(){}
+	public function __sessionFilePath(){
+		return null;
+	}
+	public function unserialize(){}
+	public function flush(){}
+}
+?>
